43b4a012e0163fe23e558260ec5edc76b9669d82
MaxA <to.altor@gmail.com>
2018-11-28 13:33:34 +0300

    Compare MealTo by JSON string

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -6,13 +6,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 import ru.javawebinar.topjava.web.SecurityUtil;
 import ru.javawebinar.topjava.web.converters.StringToLocalDate;
 import ru.javawebinar.topjava.web.converters.StringToLocalTime;
-import ru.javawebinar.topjava.web.json.JsonUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -27,9 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.javawebinar.topjava.MealTestData.*;
-import static ru.javawebinar.topjava.TestUtil.readFromJson;
-import static ru.javawebinar.topjava.TestUtil.readListFromJson;
-import static ru.javawebinar.topjava.TestUtil.contentJson;
+import static ru.javawebinar.topjava.TestUtil.*;
+import static ru.javawebinar.topjava.web.json.JsonUtil.writeValue;
 
 
 class MealRestControllerTest extends AbstractControllerTest {
@@ -60,7 +57,7 @@ class MealRestControllerTest extends AbstractControllerTest {
         Meal expected = getCreated();
         ResultActions action = mockMvc.perform(post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(expected)))
+                .content(writeValue(expected)))
                 .andExpect(status().isCreated());
 
         Meal returned = readFromJson(action, Meal.class);
@@ -75,81 +72,80 @@ class MealRestControllerTest extends AbstractControllerTest {
         Meal expected = getUpdated();
         mockMvc.perform(put(REST_URL + expected.getId())
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(expected)))
+                .content(writeValue(expected)))
                 .andExpect(status().isNoContent());
         assertMatch(mealService.get(expected.getId(), SecurityUtil.authUserId()), expected);
     }
 
     @Test
     void testGetAll() throws Exception {
-        ResultActions action = mockMvc.perform(get(REST_URL));
-        List<MealTo> actual = readListFromJson(action, MealTo.class);
-        assertEquals(actual, List.of(
+        String actual = getContent(mockMvc.perform(get(REST_URL)));
+        String expected = writeValue(List.of(
                 MealsUtil.createWithExcess(MEAL6, true),
                 MealsUtil.createWithExcess(MEAL5, true),
                 MealsUtil.createWithExcess(MEAL4, true),
                 MealsUtil.createWithExcess(MEAL3, false),
                 MealsUtil.createWithExcess(MEAL2, false),
                 MealsUtil.createWithExcess(MEAL1, false)));
+        assertEquals(expected, actual);
     }
 
     @Test
     void testGetBetweenAllFullParams() throws Exception {
         LocalDateTime start = LocalDateTime.of(2015, 5, 30, 20, 0);
         LocalDateTime end = LocalDateTime.of(2015, 6, 30, 20, 0);
-        ResultActions action = mockMvc.perform(get(REST_URL + "?" + datesToParams(
+        String actual = getContent(mockMvc.perform(get(REST_URL + "?" + datesToParams(
                 start.toLocalDate(),
                 start.toLocalTime(),
                 end.toLocalDate(),
                 end.toLocalTime()
-        )));
-        List<MealTo> actual = readListFromJson(action, MealTo.class);
-        assertEquals(actual, List.of(
+        ))));
+        String expected = writeValue(List.of(
                 MealsUtil.createWithExcess(MEAL6, true),
                 MealsUtil.createWithExcess(MEAL3, false)));
+        assertEquals(expected, actual);
     }
 
     @Test
     void testGetBetweenAllOnlyDate() throws Exception {
-        ResultActions action = mockMvc.perform(get(REST_URL + "?" + datesToParams(
+        String actual = getContent(mockMvc.perform(get(REST_URL + "?" + datesToParams(
                 LocalDate.of(2015, 5, 30),
                 null,
                 LocalDate.of(2015, 5, 30),
                 null
-        )));
-        List<MealTo> actual = readListFromJson(action, MealTo.class);
-        assertEquals(actual, List.of(
+        ))));
+        String expected = writeValue(List.of(
                 MealsUtil.createWithExcess(MEAL3, false),
                 MealsUtil.createWithExcess(MEAL2, false),
                 MealsUtil.createWithExcess(MEAL1, false)));
+        assertEquals(expected, actual);
     }
 
     @Test
     void testGetBetweenAllOnlyTime() throws Exception {
-        ResultActions action = mockMvc.perform(get(REST_URL + "?" + datesToParams(
+        String actual = getContent(mockMvc.perform(get(REST_URL + "?" + datesToParams(
                 null,
                 LocalTime.of(9, 40, 20),
                 null,
                 LocalTime.of(18, 0)
-        )));
-        List<MealTo> actual = readListFromJson(action, MealTo.class);
-        assertEquals(actual, List.of(
+        ))));
+        String expected = writeValue(List.of(
                 MealsUtil.createWithExcess(MEAL5, true),
                 MealsUtil.createWithExcess(MEAL4, true),
                 MealsUtil.createWithExcess(MEAL2, false),
                 MealsUtil.createWithExcess(MEAL1, false)));
+        assertEquals(expected, actual);
     }
 
     @Test
     void testGetBetweenAllHalfDateHalfTime() throws Exception {
-        ResultActions action = mockMvc.perform(get(REST_URL + "?" + datesToParams(
+        String actual = getContent(mockMvc.perform(get(REST_URL + "?" + datesToParams(
                 null,
                 LocalTime.of(14, 0),
                 LocalDate.of(2015, 5, 30),
                 null
-        )));
-        List<MealTo> actual = readListFromJson(action, MealTo.class);
-        assertEquals(actual, Collections.singletonList(MealsUtil.createWithExcess(MEAL3, false)));
+        ))));
+        assertEquals(writeValue(Collections.singletonList(MealsUtil.createWithExcess(MEAL3, false))), actual);
     }
 
     private static String datesToParams(LocalDate startDate,
